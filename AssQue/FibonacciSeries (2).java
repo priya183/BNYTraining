d2e3d4dfe72569aac56ecd1529b63bd83d75b6e6
Priya Thakre <100347553+priya183@users.noreply.github.com>
2022-12-14 02:32:55 +0530

    BNY Training Data

---
+++ AssQue/FibonacciSeries (2).java
@@ -0,0 +1,22 @@
+package com.yash;
+
+public class FibonacciSeries {
+
+	public String fibonacciSeries(int num) {
+		int n1=0;				
+		int n2=1;
+        int n3=0;  
+        
+        String str=n1+" "+n2;  //0 1
+		    
+		 for(int i=2;i<num;++i)  
+		 {    
+		  n3=n1+n2;   
+		  str+=" "+n3;   
+		  n1=n2;//1    
+		  n2=n3;//1    
+		 } 
+		 
+		 return str;
+	}
+}
---
+++ YashManagement/src/main/java/com/yash/model/Employee.java
@@ -0,0 +1,49 @@
+package com.yash.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Employee {
+	@Id
+	private int empId;
+	private String empName;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+	}
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
